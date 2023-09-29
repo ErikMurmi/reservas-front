8b4ffd1a4ecf70191b51cd1ab56d81e0ed3605b3
ErikMurmi <erikmurmi@gmail.com>
2023-09-29 14:41:53 -0500

    Added nav bar position

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -31,8 +27,8 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
+          src="/images/logo_udla.png"
+          alt="UDLA Logo"
           width={180}
           height={37}
           priority
@@ -91,5 +87,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
